80e83e9993e0e751262e269ecda049acb574f49b
Uros Jankovic <uros.jankovic@wundermanthompson.com>
2024-11-10 11:03:52 +0100

    Evo to ga na!
    - Malo ja Bobanu da uradim domaci!

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,57 +1,51 @@
-
 <template>
- 
-  <div class="card"  style="width: 18rem;">
-  <img src="../assets/logo.svg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">{{ card.title }}</h5>
-    <p v-if="show" class="card-text">{{ card.text }} </p>
-    <button @click="show=!show">toggle</button>
-    <button @click="remove" class="btn btn-danger">Delete></button>
-    
+
+  <div class="card"
+       style="width: 18rem;">
+    <img src="../assets/logo.svg"
+         class="card-img-top"
+         alt="...">
+    <div class="card-body">
+      <h5 class="card-title">{{ card.title }}</h5>
+      <p v-if="show"
+         class="card-text">{{ card.text }} </p>
+      <button @click="show = !show">toggle</button>
+      <button @click="remove"
+              class="btn btn-danger">Delete></button>
+
+    </div>
   </div>
-</div>
-  
+
 </template>
 
 <script>
 export default {
-name: 'card',
-props: {
-  emits: ['remove'],
-  card:{
-    type : Object,
-    Required: true,
-    default: () => ({})
+  name: 'card',
+  props: {
+    emits: ['remove'],
+    card: {
+      type: Object,
+      Required: true,
+      default: () => ({})
+    },
+    index: {
+      type: Number,
+      Required: true
+    }
   },
-  index :{
-    type : Number,
-    Required :true
+
+  data() {
+
+    return {
+      show: true
+    }
+  },
+  methods: {
+    remove() {
+      console.log('Index on child!', this.index)
+      this.$emit('remove', this.index)
+    }
+
   }
-},
-
-data (){
-
-  return{
-    show: true
-  }
-},
-methods : {
-remove(){
-  this.$('remove', this.index)
 }
-
-}
-
-
-
-
-}
-
-
-
-
-
-
 </script>
-
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,57 +1,52 @@
 <template>
-
   <header>
-    
-
     <div class="wrapper">
-     <div class="container row">
-      <card v-for="c in cards" 
-      class="4" :card="(c,i)"
-      :index="i"
-       @remove="removeCard" ></card>
-     </div>
-
-      
+      <div class="container row">
+        <card v-for="(c, i) in cards"
+              class="4"
+              :card="c"
+              :index="i"
+              @remove="removeCard"></card>
+      </div>
     </div>
   </header>
-    <div>
-<h3>home page</h3>
-</div>
+
+  <div>
+    <h3>home page</h3>
+  </div>
 </template>
 
 <script>
 import card from '../components/card.vue';
 export default {
-name : 'homeView',
-
-
-
-  components : {
-card
- 
+  name: 'homeView',
+  components: {
+    card
   },
-  data (){
-    return{
-      cards : [ 
-  {
- title : 'naslov 1',
-  text : 'texs,text,text',
-},
-{
-  title : 'naslov 2',
-  text : 'texs,text,text',
-},
-{
-  title : 'naslov 3',
-  text : 'texs,text,text',
-}
-]
+
+  data() {
+    return {
+      cards: [
+        {
+          title: 'naslov 1',
+          text: 'texs,text,text',
+        },
+        {
+          title: 'naslov 2',
+          text: 'texs,text,text',
+        },
+        {
+          title: 'naslov 3',
+          text: 'texs,text,text',
+        }
+      ]
     }
   },
+
   methods: {
-removeCard(){
-this.cards.splice (index,1)
-}
+    removeCard(index) {
+      this.cards.splice(index, 1)
+    }
 
   }
 }
